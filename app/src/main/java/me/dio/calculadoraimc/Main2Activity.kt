832162852e317c35efec5cdac90481143c525291
Pedro <phptrebil@gmail.com>
2022-07-15 22:05:02 -0300

    first commit

---
+++ app/src/main/java/me/dio/calculadoraimc/Main2Activity.kt
@@ -0,0 +1,33 @@
+package me.dio.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main2.*
+
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setListeners()
+
+    }
+
+    private fun setListeners(){
+        my_button?.setOnClickListener {
+            CalcularIMC(weight.text.toString(), high.text.toString())
+        }
+    }
+
+    private fun CalcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso !=null && altura!= null){
+            val imc = peso / (altura * altura /10000)
+            title2.text = "seu IMC é de $imc".format(imc)
+
+        }
+    }
+
+}
